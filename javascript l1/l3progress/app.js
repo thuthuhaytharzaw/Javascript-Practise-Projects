264c910f236eb8b80e9c06a9ac43bc5817532e48
thu thu hay thar zaw <thuhaytharzaw2021@gmail.com>
2024-01-31 08:08:48 +0630

    progress

---
+++ javascript l1/l3progress/app.js
@@ -0,0 +1,23 @@
+var getdownloadbtn = document.querySelector('.download-btn');
+var getprogressbar = document.querySelector('.progress-bar');
+
+getdownloadbtn.addEventListener("click",function(){
+
+    getdownloadbtn.setAttribute('disabled',true);
+
+    var setwidth = 0;
+    var setinv = setInterval(progressinc, 100);
+
+    function progressinc(){
+        if(setwidth >= 100){
+            clearInterval(setinv);
+            setwidth = 0;
+        }else{
+            setwidth++;
+
+            getprogressbar.style.width = `${setwidth}%`;
+            getprogressbar.setAttribute('data-inc', `${setwidth}%`);
+        }
+    };
+
+});
