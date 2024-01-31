71c2bb07bdf3417d5299b446a0d51e05cb6f013f
thu thu hay thar zaw <thuhaytharzaw2021@gmail.com>
2024-01-31 19:13:00 +0630

    scrollprogress

---
+++ javascript l1/l2scrollprogress/app.js
@@ -0,0 +1,9 @@
+var getprogressbar = document.getElementById("progress-bar");
+
+window.onscroll = function(){scrollpoint()};
+
+function scrollpoint(){
+    // console.log('i am working');
+
+    
+}
